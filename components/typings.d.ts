65babf626e402b8ee792516e2371c802dd3e5247
兰天游 <dtlantianyou@gmail.com>
2021-08-21 23:02:54 +0800

    API and type

---
+++ components/typings.d.ts
@@ -0,0 +1,4 @@
+export type Data = {
+  email: string,
+  name: string
+}
---
+++ pages/api/emails.ts
@@ -0,0 +1,20 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import type { Data } from '../../components/typings'
+
+const apiEndPoint = "https://l94wc2001h.execute-api.ap-southeast-2.amazonaws.com/prod/fake-auth"
+
+export const mockData: Data = {
+  name: "abc",
+  email: "lantianyou@gmail.com"
+}
+
+export default async function emailHandler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  const response = await fetch(apiEndPoint, {
+    method: req.method,
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: req.body
+  })
+  res.status(response.status)
+}
